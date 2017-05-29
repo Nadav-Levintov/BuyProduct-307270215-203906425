0d94ea9918f2658df85c8f2d86c34e7bff47a1b3
Nadav-Levintov <dsp155@gmail.com>
2017-05-29 13:01:02 +0300

    Finished App

--- buy-app/src/main/java/il/ac/technion/cs/sd/buy/app/BuyProductReaderImpl.java
+++ buy-app/src/main/java/il/ac/technion/cs/sd/buy/app/BuyProductReaderImpl.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import com.google.inject.internal.cglib.core.$CollectionUtils;
 import db_utils.DataBase;
 import db_utils.DataBaseFactory;
+import javafx.util.Pair;
 
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
@@ -349,7 +350,7 @@ public class BuyProductReaderImpl implements BuyProductReader {
                 .stream()
                 .map(line -> {
                     String arr[] = line.split(",");
-                    return arr[0];
+                    return arr[0]; //order_id
                 })
                 .distinct()
                 .collect(Collectors.toList()));
@@ -359,31 +360,327 @@ public class BuyProductReaderImpl implements BuyProductReader {
 
     @Override
     public CompletableFuture<OptionalLong> getTotalNumberOfItemsPurchased(String productId) {
-        return null;
+        CompletableFuture<List<Long>> res_list = CompletableFuture.completedFuture(new ArrayList<>());
+        CompletableFuture<Long> sum = CompletableFuture.completedFuture(new Long(0));
+
+        List<String> names_of_keys = new ArrayList<>();
+        names_of_keys.add("product");
+        List<String> keys = new ArrayList<>();
+        keys.add(productId);
+
+        CompletableFuture<List<String>> order_line_list = ordersDB.thenCompose(orders -> orders
+                .get_lines_for_keys(names_of_keys,keys));
+
+        res_list = order_line_list.thenCompose(lines ->
+                {
+                    List<CompletableFuture<Long>> temp_list =lines.stream()
+                            .map(line -> {
+                                String line_values[] = line.split(",");
+                                String order_id = line_values[0];
+                                String user_id = line_values[1];
+                                String amount = line_values[2];
+                                Boolean is_canceled = Boolean.parseBoolean(line_values[4]);
+                                Boolean is_modified = Boolean.parseBoolean(line_values[3]);
+
+                                CompletableFuture<Long> res = CompletableFuture.completedFuture(new Long(0));
+                                if (!is_canceled) {
+                                    if (is_modified) {
+                                        List<String> names_of_keys_mod = new ArrayList<>();
+                                        names_of_keys_mod.add("order");
+                                        List<String> keys_mod = new ArrayList<>();
+                                        keys_mod.add(order_id);
+                                        CompletableFuture<List<String>> mod_order_line_list = modified_ordersDB.thenCompose(mod_orders -> mod_orders
+                                                .get_lines_for_keys(names_of_keys_mod, keys_mod));
+
+                                        res = mod_order_line_list.thenApply(mod_list -> Long.parseLong(mod_list.get(mod_list.size() - 1).split(",")[0]));
+                                    } else {
+                                        res = CompletableFuture.completedFuture(Long.parseLong(amount));
+                                    }
+                                }
+
+
+                                return res;
+                            })
+                            .collect(Collectors.toList());
+
+                    return CompletableFuture.allOf(temp_list.toArray(new CompletableFuture[temp_list.size()]))
+                            .thenApply(v -> temp_list.stream()
+                                    .map(CompletableFuture::join)
+                                    .collect(Collectors.toList()));
+                } );
+
+        sum =res_list.thenApply(list -> list.stream().mapToLong(i -> i.longValue()).sum());
+
+        return sum.thenApply(val ->
+        {
+            if(val.equals(new Long(0)))
+            {
+                return OptionalLong.empty();
+            }
+            else
+            {
+                return OptionalLong.of(val);
+            }
+        });
     }
 
     @Override
     public CompletableFuture<OptionalDouble> getAverageNumberOfItemsPurchased(String productId) {
-        return null;
+
+        CompletableFuture<OptionalDouble> avg = CompletableFuture.completedFuture(OptionalDouble.empty());
+
+        CompletableFuture<OptionalLong> numberOfItemsPurchased = getTotalNumberOfItemsPurchased(productId);
+        CompletableFuture<List<String>> order_ids = getOrderIdsThatPurchased(productId);
+        CompletableFuture<Integer> numOfOrders = order_ids.thenApply(List::size);
+
+        avg= numberOfItemsPurchased.thenCombine(numOfOrders, (purchased_amount,orders_num) -> {
+            if(orders_num == 0)
+            {
+                return OptionalDouble.empty();
+            }
+            else
+            {
+               return OptionalDouble.of((double) (purchased_amount.getAsLong() / orders_num));
+            }
+        });
+
+        return avg;
+
     }
 
     @Override
     public CompletableFuture<OptionalDouble> getCancelRatioForUser(String userId) {
-        return null;
+        CompletableFuture<OptionalDouble> ratio = CompletableFuture.completedFuture(OptionalDouble.empty());
+        CompletableFuture<Long> sum = CompletableFuture.completedFuture(new Long(0));
+
+        List<String> names_of_keys = new ArrayList<>();
+        names_of_keys.add("user");
+        List<String> keys = new ArrayList<>();
+        keys.add(userId);
+
+        CompletableFuture<List<String>> order_line_list = ordersDB.thenCompose(orders -> orders
+                .get_lines_for_keys(names_of_keys,keys));
+
+        ratio = order_line_list.thenApply(lines ->
+        {
+            if(lines.isEmpty())
+            {
+                return OptionalDouble.empty();
+            }
+
+            return OptionalDouble.of(lines.stream()
+                    .mapToDouble(line -> {
+                        String line_values[] = line.split(",");
+                        Boolean is_canceled = Boolean.parseBoolean(line_values[4]);
+
+                        Double res = new Double(0);
+                        if (is_canceled) {
+                            res = (double) 1 / lines.size();
+                        }
+                        return res;
+                    })
+                    .sum());
+        });
+
+        return ratio;
     }
 
     @Override
     public CompletableFuture<OptionalDouble> getModifyRatioForUser(String userId) {
-        return null;
+        CompletableFuture<OptionalDouble> ratio = CompletableFuture.completedFuture(OptionalDouble.empty());
+        CompletableFuture<Long> sum = CompletableFuture.completedFuture(new Long(0));
+
+        List<String> names_of_keys = new ArrayList<>();
+        names_of_keys.add("user");
+        List<String> keys = new ArrayList<>();
+        keys.add(userId);
+
+        CompletableFuture<List<String>> order_line_list = ordersDB.thenCompose(orders -> orders
+                .get_lines_for_keys(names_of_keys,keys));
+
+        ratio = order_line_list.thenApply(lines ->
+        {
+            if(lines.isEmpty())
+            {
+                return OptionalDouble.empty();
+            }
+
+            return OptionalDouble.of(lines.stream()
+                    .mapToDouble(line -> {
+                        String line_values[] = line.split(",");
+                        Boolean is_moded = Boolean.parseBoolean(line_values[3]);
+
+                        Double res = new Double(0);
+                        if (is_moded) {
+                            res = (double) 1 / lines.size();
+                        }
+                        return res;
+                    })
+                    .sum());
+        });
+
+        return ratio;
     }
 
     @Override
     public CompletableFuture<Map<String, Long>> getAllItemsPurchased(String userId) {
-        return null;
+
+        CompletableFuture<Map<String, Long>> res_map = CompletableFuture.completedFuture(new TreeMap<>());
+        List<String> names_of_keys = new ArrayList<>();
+        names_of_keys.add("user");
+        List<String> keys = new ArrayList<>();
+        keys.add(userId);
+
+        CompletableFuture<List<String>> order_line_list = ordersDB.thenCompose(orders -> orders
+                .get_lines_for_keys(names_of_keys,keys));
+
+
+        CompletableFuture<List<Pair<CompletableFuture<String>,CompletableFuture<Long>>>> pair_list= order_line_list.thenApply(lines ->
+        {
+            return lines.stream()
+                    .map(line -> {
+                        String line_values[] = line.split(",");
+                        String order_id = line_values[0];
+                        String product_id = line_values[1];
+                        CompletableFuture<Long> curr_amount = CompletableFuture.completedFuture(Long.parseLong(line_values[2]));
+                        Boolean is_moded = Boolean.parseBoolean(line_values[3]);
+                        Boolean is_canceled = Boolean.parseBoolean(line_values[4]);
+
+
+                        if (!is_canceled) {
+                            if(is_moded)
+                            {
+                                List<String> names_of_keys_mod = new ArrayList<>();
+                                names_of_keys_mod.add("order");
+                                List<String> keys_mod = new ArrayList<>();
+                                keys_mod.add(order_id);
+
+                                CompletableFuture<List<String>> moded_line_list = ordersDB.thenCompose(orders -> orders
+                                        .get_lines_for_keys(names_of_keys_mod,keys_mod));
+
+                                curr_amount = moded_line_list.thenApply(mod_list -> Long.parseLong(mod_list.get(mod_list.size() - 1).split(",")[0]));
+
+                            }
+                        }
+                        return new Pair<CompletableFuture<String>,CompletableFuture<Long>>(CompletableFuture.completedFuture(product_id),curr_amount);
+                    })
+                    .collect(Collectors.toList());
+        });
+
+        res_map = pair_list.thenCompose(list ->
+        {
+            List<CompletableFuture<String>> key_list = list.stream().map(pair -> pair.getKey()).collect(Collectors.toList());
+            List<CompletableFuture<Long>> val_list = list.stream().map(pair -> pair.getValue()).collect(Collectors.toList());
+
+            CompletableFuture<List<String>> completableFuture_key_list = CompletableFuture.allOf(key_list.toArray(new CompletableFuture[key_list.size()]))
+                    .thenApply(v -> key_list.stream()
+                            .map(CompletableFuture::join)
+                            .collect(Collectors.toList()));
+
+            CompletableFuture<List<Long>> completableFuture_val_list = CompletableFuture.allOf(val_list.toArray(new CompletableFuture[val_list.size()]))
+                    .thenApply(v -> val_list.stream()
+                            .map(CompletableFuture::join)
+                            .collect(Collectors.toList()));
+
+            return completableFuture_key_list.thenCombine(completableFuture_val_list,(keys_list,values_list) -> {
+                Map<String,Long> temp_map = new TreeMap<>();
+
+                for(int i=0;i<key_list.size();i++)
+                {
+                    String curr_key = keys_list.get(i);
+                    Long curr_val = values_list.get(i);
+                    if(temp_map.containsKey(curr_key))
+                    {
+                        curr_val+=temp_map.get(curr_key);
+                    }
+                    temp_map.put(curr_key,curr_val);
+                }
+                return temp_map;
+            });
+
+        });
+
+        return res_map;
+
     }
 
     @Override
     public CompletableFuture<Map<String, Long>> getItemsPurchasedByUsers(String productId) {
-        return null;
+        CompletableFuture<Map<String, Long>> res_map = CompletableFuture.completedFuture(new TreeMap<>());
+        List<String> names_of_keys = new ArrayList<>();
+        names_of_keys.add("product");
+        List<String> keys = new ArrayList<>();
+        keys.add(productId);
+
+        CompletableFuture<List<String>> order_line_list = ordersDB.thenCompose(orders -> orders
+                .get_lines_for_keys(names_of_keys,keys));
+
+
+        CompletableFuture<List<Pair<CompletableFuture<String>,CompletableFuture<Long>>>> pair_list= order_line_list.thenApply(lines ->
+        {
+            return lines.stream()
+                    .map(line -> {
+                        String line_values[] = line.split(",");
+                        String order_id = line_values[0];
+                        String user_id = line_values[1];
+                        CompletableFuture<Long> curr_amount = CompletableFuture.completedFuture(Long.parseLong(line_values[2]));
+                        Boolean is_moded = Boolean.parseBoolean(line_values[3]);
+                        Boolean is_canceled = Boolean.parseBoolean(line_values[4]);
+
+
+                        if (!is_canceled) {
+                            if(is_moded)
+                            {
+                                List<String> names_of_keys_mod = new ArrayList<>();
+                                names_of_keys_mod.add("order");
+                                List<String> keys_mod = new ArrayList<>();
+                                keys_mod.add(order_id);
+
+                                CompletableFuture<List<String>> moded_line_list = ordersDB.thenCompose(orders -> orders
+                                        .get_lines_for_keys(names_of_keys_mod,keys_mod));
+
+                                curr_amount = moded_line_list.thenApply(mod_list -> Long.parseLong(mod_list.get(mod_list.size() - 1).split(",")[0]));
+
+                            }
+                        }
+                        return new Pair<CompletableFuture<String>,CompletableFuture<Long>>(CompletableFuture.completedFuture(user_id),curr_amount);
+                    })
+                    .collect(Collectors.toList());
+        });
+
+        res_map = pair_list.thenCompose(list ->
+        {
+            List<CompletableFuture<String>> key_list = list.stream().map(pair -> pair.getKey()).collect(Collectors.toList());
+            List<CompletableFuture<Long>> val_list = list.stream().map(pair -> pair.getValue()).collect(Collectors.toList());
+
+            CompletableFuture<List<String>> completableFuture_key_list = CompletableFuture.allOf(key_list.toArray(new CompletableFuture[key_list.size()]))
+                    .thenApply(v -> key_list.stream()
+                            .map(CompletableFuture::join)
+                            .collect(Collectors.toList()));
+
+            CompletableFuture<List<Long>> completableFuture_val_list = CompletableFuture.allOf(val_list.toArray(new CompletableFuture[val_list.size()]))
+                    .thenApply(v -> val_list.stream()
+                            .map(CompletableFuture::join)
+                            .collect(Collectors.toList()));
+
+            return completableFuture_key_list.thenCombine(completableFuture_val_list,(keys_list,values_list) -> {
+                Map<String,Long> temp_map = new TreeMap<>();
+
+                for(int i=0;i<key_list.size();i++)
+                {
+                    String curr_key = keys_list.get(i);
+                    Long curr_val = values_list.get(i);
+                    if(temp_map.containsKey(curr_key))
+                    {
+                        curr_val+=temp_map.get(curr_key);
+                    }
+                    temp_map.put(curr_key,curr_val);
+                }
+                return temp_map;
+            });
+
+        });
+
+        return res_map;
     }
 }
